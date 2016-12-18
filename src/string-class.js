b4660b9c2c2aceaa5c30afa417100ffff07a5454
andela-nekekwe <ngozi.ekekwe@andela.com>
2016-12-18 23:23:00 +0100

    feature(string-class): write methods to extend string class

--- src/string-class.js
+++ src/string-class.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const stringManipulator = {
 
   hasVowels() {
@@ -23,12 +25,75 @@ const stringManipulator = {
   },
 
   isQuestion() {
-    return /[?]$/i.test(this);
+    return /[?]$/i.test(this.removeSpace());
+  },
+
+  removeSpace() {
+    return this.replace(/^\s+|\s+$/g, '');
   },
 
   words() {
-    return this.split(/\s/);
+    return this.replace(/[^\w\s+]/gi, '').split(/\s+/);
   },
+
+  wordCount() {
+    let count = 0;
+    const arr = this.words();
+    arr.forEach((item, index) => {
+      count += index;
+    });
+    return count;
+  },
+
+  toCurrency() {
+    return this.replace(/(\d+)(?=(\d{3})+(?!\d))/g, '$1,');
+  },
+
+  fromCurrency() {
+    return parseFloat((this.replace(/,+/g, '')));
+  },
+
+  inverseCase() {
+    return this.replace(/\w/g, found =>
+    /[A-Z]/g.test(found) ? found.toLower() : found.toUpper()
+    );
+  },
+  alternatingCase() {
+    return this.replace(/\w/g, (found, index) =>
+    (index % 2 === 0) ? found.toLower() : found.toUpper()
+    );
+  },
+
+  getMiddle() {
+    const length = this.length;
+    const middle = Math.floor(length / 2);
+    return (middle % 2 === 0) ? this[middle - 1] + this[middle] : this[middle];
+  },
+  numberWords() {
+    const words = {
+      0: 'zero',
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine'
+    };
+    return this.replace(/\d/g, (found, index) =>
+    (index === 0) ? words[found] : ` ${words[found]}`
+    );
+  },
+
+  isDigit() {
+    return /^[\d]$/.test(this);
+  },
+
+  doubleCheck() {
+    return /(.)\1/.test(this);
+  }
 
 };
 Object.assign(String.prototype, stringManipulator);
