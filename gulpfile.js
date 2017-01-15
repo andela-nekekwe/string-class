06c498452ed0858486c13ecb0ec4994d1eaae754
andela-nekekwe <ngozi.ekekwe@andela.com>
2017-01-16 00:28:15 +0100

    fix hound violations

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
+
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const rename = require('gulp-rename');
 
-gulp.task('bundle-app', function () {
+gulp.task('bundle-app', () => {
   return gulp.src('src/string-class.js')
     .pipe(webpack(require('./webpack.config.js')))
     .pipe(rename({
-      basename: "bundle"
+      basename: 'bundle'
     }))
     .pipe(gulp.dest('./public/'));
 });
